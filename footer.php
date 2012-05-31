688598356c4dceaaa51c98c2b0583d8b5222e74a
Matt Hand <matt.kaze3@gmail.com>
2012-05-31 22:30:07 +0100

    Add footer code and register widgets in functions.php.

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div id="footer">
+  <div id="footer-1">
+    <?php dynamic_sidebar('Footer 1'); ?>
+  </div>
+  <div id="footer-2">
+    <?php dynamic_sidebar('Footer 2'); ?>
+  </div>
+  <div id="colophon">
+    <p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
+  </div>
+</div>  <!-- footer -->
+
+</div>  <!-- site -->
+</div>  <!-- wrap -->
+<?php wp_footer(); ?>
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
+$content_width = 580;
+
+if (function_exists('register_sidebar')) {
+  register_sidebar(array('name'=>'Footer 1'));
+  register_sidebar(array('name'=>'Footer 2'));
+}
+
 function register_menus() {
   register_nav_menus(
     array('header-menu' => 'Header Menu')
