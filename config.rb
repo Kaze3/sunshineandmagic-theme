944627d2c59842a9ba1af375276990ddf544909b
Matt Hand <matt.kaze3@gmail.com>
2012-06-01 20:47:58 +0100

    Revert http path and relative assets. Change image path.

--- config.rb
+++ config.rb
@@ -2,17 +2,17 @@
 require 'ninesixty'
 
 # Set this to the root of your project when deployed:
-http_path = "/wp-content/themes/sunshineandmagic-theme/"
+http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "wp-content/themes/sunshineandmagic-theme/images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
