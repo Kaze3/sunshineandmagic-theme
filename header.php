09a64a7293b374de43935ed1c1b3fa36518753d5
Matt Hand <matt.kaze3@gmail.com>
2012-06-01 11:45:23 +0100

    Fix stray closing tag in head. Remove specification of separator position (not used).

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <! DOCTYPE html>
 <html>
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>" /> />
-  <title><?php wp_title('', true, 'right'); ?> | <?php bloginfo('name'); ?></title>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <title><?php wp_title('', true); ?> | <?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
   <?php wp_head(); ?>
