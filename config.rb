cf1b314a86f5a5f36612125a8aaa0a92314d8ef5
Matt Hand <matt.kaze3@gmail.com>
2012-06-01 21:03:50 +0100

    Fix relative image url.

--- config.rb
+++ config.rb
@@ -5,14 +5,14 @@ require 'ninesixty'
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "wp-content/themes/sunshineandmagic-theme/images"
+images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
