e68777566157eb59b5a3788794058b2a293bf74d
Matt Hand <matt.kaze3@gmail.com>
2012-06-02 10:49:42 +0100

    Fix submenu listing.

--- header.php
+++ header.php
@@ -21,7 +21,10 @@
     if ($post->post_parent) {
       $ancestors=get_post_ancestors($post->ID);
       $root=count($ancestors)-1;
-      $parent = $ancestors[$root]; ?>
+      $parent = $ancestors[$root];
+    } else {
+      $parent = $post->ID;
+    } ?>
 
   <div id="header-submenu">
     <ul>
