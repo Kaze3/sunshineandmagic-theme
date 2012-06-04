fa489c263db93c4d15a36ed6f30439452e0fcb7b
Matt Hand <matt.kaze3@gmail.com>
2012-06-04 15:26:23 +0100

    Change execution priority of wpautop and execute from within shortcoes.

--- functions.php
+++ functions.php
@@ -13,32 +13,35 @@ function register_menus() {
 }
 add_action('init', 'register_menus');
 
+remove_filter( 'the_content', 'wpautop' );
+add_filter( 'the_content', 'wpautop' , 12);
+
 function strip_empty_p($content) {
   return str_replace("\r\n", '', $content);
 }
 
 function grid_6_alpha_shortcode($atts, $content = null) {
-  return '<div class="container_12"><div class="grid_6 alpha">' . strip_empty_p($content) . '</div>';
+  return '<div class="container_12"><div class="grid_6 alpha">' . wpautop(trim($content)) . '</div>';
 }
 add_shortcode('2col_first', 'grid_6_alpha_shortcode');
 
 function grid_6_omega_shortcode($atts, $content = null) {
-  return '<div class="grid_6 omega">' . strip_empty_p($content) . '</div></div>';
+  return '<div class="grid_6 omega">' . wpautop(trim($content)) . '</div></div>';
 }
 add_shortcode('2col_second', 'grid_6_omega_shortcode');
 
 function grid_4_alpha_shortcode($atts, $content = null) {
-  return '<div class="container_12"><div class="grid_4 alpha">' . strip_empty_p($content) . '</div>';
+  return '<div class="container_12"><div class="grid_4 alpha">' . wpautop(trim($content)) . '</div>';
 }
 add_shortcode('3col_first', 'grid_4_alpha_shortcode');
 
 function grid_4_shortcode($atts, $content = null) {
-  return '<div class="grid_4">' . strip_empty_p($content) . '</div>';
+  return '<div class="grid_4">' . wpautop(trim($content)) . '</div>';
 }
 add_shortcode('3col_second', 'grid_4_shortcode');
 
 function grid_4_omega_shortcode($atts, $content = null) {
-  return '<div class="grid_4 omega">' . strip_empty_p($content) . '</div></div>';
+  return '<div class="grid_4 omega">' . wpautop(trim($content)) . '</div></div>';
 }
 add_shortcode('3col_third', 'grid_4_omega_shortcode');
 ?>
