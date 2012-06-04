e5db448581b381b98b28d97449a67b1f5c370ae4
Matt Hand <matt.kaze3@gmail.com>
2012-06-04 14:55:38 +0100

    Add p stripping function.

--- functions.php
+++ functions.php
@@ -13,32 +13,36 @@ function register_menus() {
 }
 add_action('init', 'register_menus');
 
+function strip_empty_p($string) {
+  return preg_replace('/<p><\/p>/', '', $string);
+}
+
 function grid_6_alpha_shortcode($atts, $content = null) {
-  $content = preg_replace('<p></p>', '', $content);
+  $content = strip_empty_p($content);
   return '<div class="container_12"><div class="grid_6 alpha">' . $content . '</div>';
 }
 add_shortcode('2col_first', 'grid_6_alpha_shortcode');
 
 function grid_6_omega_shortcode($atts, $content = null) {
-  $content = preg_replace('<p></p>', '', $content);
+  $content = strip_empty_p($content);
   return '<div class="grid_6 omega">' . $content . '</div></div>';
 }
 add_shortcode('2col_second', 'grid_6_omega_shortcode');
 
 function grid_4_alpha_shortcode($atts, $content = null) {
-  $content = preg_replace('<p></p>', '', $content);
+  $content = strip_empty_p($content);
   return '<div class="container_12"><div class="grid_4 alpha">' . $content . '</div>';
 }
 add_shortcode('3col_first', 'grid_4_alpha_shortcode');
 
 function grid_4_shortcode($atts, $content = null) {
-  $content = preg_replace('<p></p>', '', $content);
+  $content = strip_empty_p($content);
   return '<div class="grid_4">' . $content . '</div>';
 }
 add_shortcode('3col_second', 'grid_4_shortcode');
 
 function grid_4_omega_shortcode($atts, $content = null) {
-  $content = preg_replace('<p></p>', '', $content);
+  $content = strip_empty_p($content);
   return '<div class="grid_4 omega">' . $content . '</div></div>';
 }
 add_shortcode('3col_third', 'grid_4_omega_shortcode');
