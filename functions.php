5cdf963a3f3c47498d76357afde69eea0ade840f
Matt Hand <matt.kaze3@gmail.com>
2012-06-05 21:03:19 +0100

    Fix menu registration.

--- functions.php
+++ functions.php
@@ -7,8 +7,8 @@ if (function_exists('register_sidebar')) {
 
 function register_menus() {
   register_nav_menus(
-    array('header-menu' => 'Header Menu'),
-    array('footer-menu' => 'Footer Menu')
+    array('header-menu' => 'Header Menu',
+          'footer-menu' => 'Footer Menu')
   );
 }
 add_action('init', 'register_menus');
