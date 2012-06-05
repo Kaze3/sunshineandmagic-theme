f0aa5868bc346676eaa9f74d4eca0ce9c4a48281
Matt Hand <matt.kaze3@gmail.com>
2012-06-05 21:00:06 +0100

    Remove extra footer widget registration.

--- functions.php
+++ functions.php
@@ -2,8 +2,7 @@
 $content_width = 960;
 
 if (function_exists('register_sidebar')) {
-  register_sidebar(array('name'=>'Footer 1'));
-  register_sidebar(array('name'=>'Footer 2'));
+  register_sidebar(array('name'=>'Footer'));
 }
 
 function register_menus() {
