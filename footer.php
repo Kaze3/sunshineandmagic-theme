18f2635a3974b250f407192fbfce087007aeaf39
Matt Hand <matt.kaze3@gmail.com>
2012-06-05 20:58:30 +0100

    Replace one of the footer widgets with a menu.

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div id="footer">
-  <div id="footer-1">
-    <?php dynamic_sidebar('Footer 1'); ?>
+  <div id="footer-menu">
+    <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
   </div>
-  <div id="footer-2">
-    <?php dynamic_sidebar('Footer 2'); ?>
+  <div id="footer-widget">
+    <?php dynamic_sidebar('Footer'); ?>
   </div>
   <div id="colophon">
     <p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
