0ec1618b83796d9d6604ed0424be0ef1691d6d2c
Matt Hand <matt.kaze3@gmail.com>
2012-06-10 13:50:38 +0100

    Add footer container and apply grid styles.

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
-<div id="footer">
-  <div id="footer-menu">
-    <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
-  </div>
-  <div id="footer-widget">
-    <?php dynamic_sidebar('Footer'); ?>
-  </div>
-  <div id="colophon">
-    <p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
-  </div>
-</div>  <!-- footer -->
+<div id="footer-container">
+  <div id="footer">
+    <div id="footer-menu">
+      <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
+    </div>
+    <div id="footer-widget">
+      <?php dynamic_sidebar('Footer'); ?>
+    </div>
+    <div id="colophon">
+      <p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
+    </div>
+  </div>  <!-- footer -->
+</div>  <!-- footer-containter -->
 
 </div>  <!-- wrap -->
 <?php wp_footer(); ?>
