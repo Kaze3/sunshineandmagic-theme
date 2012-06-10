59cded66a252df395a0e74f582d260d6d6d2f8fb
Matt Hand <matt.kaze3@gmail.com>
2012-06-10 22:36:19 +0100

    Add FAQ custom post type.

--- functions.php
+++ functions.php
@@ -40,4 +40,37 @@ function grid_4_omega_shortcode($atts, $content = null) {
   return '<div class="grid_4 omega">' . wpautop(trim($content)) . '</div></div>';
 }
 add_shortcode('3col_third', 'grid_4_omega_shortcode');
+
+// ADDING CUSTOM POST TYPE FAQ - http://net.tutsplus.com/tutorials/wordpress/creating-an-interactive-faq-with-wordpress-and-jquery-ui/  
+add_action('init', 'my_custom_init');
+
+function my_custom_init()  
+{  
+  $labels = array(  
+    'name' => _x('FAQs', 'post type general name'),  
+    'singular_name' => _x('FAQ', 'post type singular name'),  
+    'add_new' => _x('Add New', 'FAQ'),  
+    'add_new_item' => __('Add New FAQ'),  
+    'edit_item' => __('Edit FAQ'),  
+    'new_item' => __('New FAQ'),  
+    'view_item' => __('View FAQ'),  
+    'search_items' => __('Search FAQs'),  
+    'not_found' =>  __('No FAQs found'),  
+    'not_found_in_trash' => __('No FAQs found in Trash'),  
+    'parent_item_colon' => ''  
+  );  
+  $args = array(  
+    'labels' => $labels,  
+    'public' => true,  
+    'publicly_queryable' => true,  
+    'show_ui' => true,  
+    'query_var' => true,  
+    'rewrite' => true,  
+    'capability_type' => 'post',  
+    'hierarchical' => false,  
+    'menu_position' => 5,  
+    'supports' => array('title','editor','thumbnail','custom-fields')  
+  );  
+  register_post_type('faq',$args);  
+}  
 ?>
