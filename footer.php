88681e7c4a9055d49cf6689946e787ea3c25be08
Matt Hand <matt.kaze3@gmail.com>
2012-06-10 13:59:11 +0100

    Add footer-items div.

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
 <div id="footer-container">
   <div id="footer">
-    <div id="footer-menu">
-      <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
-    </div>
-    <div id="footer-widget">
-      <?php dynamic_sidebar('Footer'); ?>
-    </div>
-    <div id="colophon">
-      <p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
-    </div>
+    <div id="footer-items">
+      <div id="footer-menu">
+        <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
+      </div>
+      <div id="footer-widget">
+        <?php dynamic_sidebar('Footer'); ?>
+      </div>
+      <div id="colophon">
+        <p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
+      </div>
+    </div>  <!-- footer-items -->
   </div>  <!-- footer -->
 </div>  <!-- footer-containter -->
 
