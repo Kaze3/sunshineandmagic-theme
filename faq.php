52ed79f8b5a7ac5783304215d575de092633580f
Matt Hand <matt.kaze3@gmail.com>
2012-06-12 22:54:36 +0100

    Completely reorganize faq query structure.

--- faq.php
+++ faq.php
@@ -13,31 +13,44 @@ Template Name: FAQ
     </header>
     <div id="questions">
 
-<?php $categories = array('payment', 'arrival', 'amenities', 'location');
+<?php
+$payment = new WP_Query(array('post_type' => 'faq', 'category_name' => 'payment'));
+if ($payment->have_posts())
+  $queries[] = $payment;
+  
+$arrival = new WP_Query(array('post_type' => 'faq', 'category_name' => 'arrival'));
+if ($arrival->have_posts())
+  $queries[] = $arrival;
+  
+$amenities = new WP_Query(array('post_type' => 'faq', 'category_name' => 'amenities'));
+if ($amenities->have_posts())
+  $queries[] = $amenities;
+  
+$location = new WP_Query(array('post_type' => 'faq', 'category_name' => 'location')); 
+if ($location->have_posts())
+  $queries[] = $location
+?>
 
-foreach ($categories as $category) {
-  $query = new WP_Query(array('post_type' => 'faq', 'category_name' => $category)); ?>
-    <?php if ($query->have_posts()) : ?>
-      <?php $query->the_post(); ?>
+<?php
+foreach ($queries as $my_query) {
+      <?php $wp_query = clone $my_query; ?>
+      <?php the_post(); ?>
       <?php $the_category = get_the_category(); ?>
         <h3><?php echo $the_category[0]->cat_name; ?></h3>
         <?php rewind_posts(); ?>
         <ul>  
-          <?php while ($query->have_posts()) : $query->the_post(); ?>  
+          <?php while (have_posts()) : the_post(); ?>  
             <li><a href="#answer-<?php echo sanitize_title(get_the_title()); ?>"><?php the_title(); ?></a></li>  
           <?php endwhile; ?>  
         </ul>     
-    <?php endif; ?>
     <?php } ?>
     </div>
     
-<?php foreach ($categories as $category) {
-  $query = new WP_Query(array('post_type' => 'faq', 'category_name' => $category)); ?>
-    
-    <?php if ($query->have_posts()) : ?>  
+foreach ($queries as $my_query) {
+<?php $wp_query = clone $my_query; ?>
       <div id="answers">  
         <ul>  
-          <?php while ($query->have_posts()) : $query->the_post(); ?>  
+          <?php while (have_posts()) : the_post(); ?>  
             <li id="answer-<?php echo sanitize_title(get_the_title()); ?>">  
               <h2><?php the_title(); ?></h2>  
               <?php the_content(); ?>  
@@ -45,7 +58,6 @@ foreach ($categories as $category) {
           <?php endwhile; ?>  
         </ul>  
       </div>  
-    <?php endif; ?>
     
 <?php } ?>  
   </div>
