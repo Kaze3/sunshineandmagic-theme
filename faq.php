a2a9817d4b913a1e734eaa985eebf7da673d5336
Matt Hand <matt.kaze3@gmail.com>
2012-06-12 17:32:49 +0100

    Don't show anything when category is empty. Fix category heading display.

--- faq.php
+++ faq.php
@@ -19,14 +19,12 @@ foreach ($categories as $category) {
   $query = new WP_Query(array('post_type' => 'faq', 'category_name' => $category)) ?>
 
     <?php if ($query->have_posts()) : ?>
-        <h3><?php echo get_the_category()->cat_name; ?></h3>
+        <h3><?php echo get_the_category(); ?></h3>
         <ul>  
           <?php while ($query->have_posts()) : $query->the_post(); ?>  
             <li><a href="#answer-<?php echo sanitize_title(get_the_title()); ?>"><?php the_title(); ?></a></li>  
           <?php endwhile; ?>  
-        </ul>    
-    <?php else : ?>
-      <p>Sorry, No FAQs created yet.</p>  
+        </ul>     
     <?php endif; ?>
     <?php } ?>
     </div>
