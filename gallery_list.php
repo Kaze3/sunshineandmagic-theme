f9adb1b25f177798ea65512ad6a6dbbe1094ab13
Matt Hand <matt.kaze3@gmail.com>
2013-01-01 15:28:21 +0000

    Add function to get first image from gallery. Output page title and image URL.

--- gallery_list.php
+++ gallery_list.php
@@ -4,6 +4,31 @@ Template Name: Gallery List
 */
 ?>
 
+<?php 
+  function gallery_first_image($post_id){
+ 
+    global $post;
+ 
+    $args = array(
+        'post_type'   => 'attachment',
+        'numberposts' => 1,
+        'post_parent' => $post_id,
+        'order' => 'ASC',
+        'orderby' => 'menu_order',
+        'post_mime_type' => 'image'
+        );
+ 
+    $attachments = get_posts( $args );
+ 
+    if ( $attachments ) {
+        foreach ( $attachments as $attachment ) {
+            return wp_get_attachment_url( $attachment->ID );
+        }
+    }
+    return false;
+  }
+?>
+
 <?php get_header(); ?>
 
 <div id="content-container">
@@ -17,10 +42,10 @@ Template Name: Gallery List
   $child_gallery_pages = get_posts($args);
 
   foreach ($child_gallery_pages as $gallery_page) {
-    echo($gallery_page->ID);
-    echo(get_the_title($gallery_page->ID));
-  }
 ?>
+  <h2><?php echo get_the_title($gallery_page->ID); ?></h2>
+  <p><?php echo gallery_first_image($gallery_page->ID); ?></p>
+<?php } ?>
 
 </div> <!-- content -->
 </div> <!-- content-container -->
