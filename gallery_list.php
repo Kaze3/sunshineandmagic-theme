1447f89584b4005c85bf474dcc3cb0ca178580b5
Matt Hand <matt.kaze3@gmail.com>
2013-01-01 14:55:55 +0000

    Add Gallery List template file and start implementation.

---
+++ gallery_list.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: Gallery List
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="content-container">
+  <div id="content">
+    <header class="entry-header">
+      <h1 class="entry-title">Gallery</h1>
+    </header>
+
+<?php
+$all_pages = new WP_Query(array('post_type' => 'page'));
+if ($all_pages->have_posts()) {
+  $child_gallery_pages = get_page_children($post->ID, $all_pages);
+
+  foreach ($child_gallery_pages as $gallery_page) {
+    echo(get_the_title($gallery_page->ID));
+  }
+}
+?>
+
+</div> <!-- content -->
+</div> <!-- content-container -->
+
+<?php get_footer(); ?>
