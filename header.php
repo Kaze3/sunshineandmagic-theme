9dc5e8951da566d43768fefafdbe40385e2569bd
Matt Hand <matt.kaze3@gmail.com>
2013-03-23 16:21:35 +0000

    Move CSS below wp_head().

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <title><?php is_front_page() ? bloginfo('description') : wp_title(''); ?> | <?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
   <?php wp_head(); ?>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 </head>
   
 <body <?php body_class(); ?>>
