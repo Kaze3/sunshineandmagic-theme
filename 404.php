e1d0bbeba3a4eb23fa97cf66b5514df238c89f6d
Matt Hand <matt.kaze3@gmail.com>
2013-03-23 12:42:40 +0000

    Add 404 page.

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div id="content-container">
+  <div id="content">
+    <header class="entry-header">
+      <h1 class="entry-title">Oops!</h1>
+    </header>
+    <div class="entry-content">
+      <p>Whatever you're looking for, it isn't here. Sorry about that. Try following one of the links above or just return to the <a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> homepage to start fresh.</p>
+    </div> <!-- entry-content -->
+  </div> <!-- content -->
+</div> <!-- content-container -->
+
+<?php get_footer(); ?>
