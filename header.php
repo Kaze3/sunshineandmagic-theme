7a718ad096899f4fbb834d99c37d2c89ce16008a
Matt Hand <matt.kaze3@gmail.com>
2013-05-02 09:00:35 +0100

    Update page titles for better SEO.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
-  <title><?php is_front_page() ? bloginfo('description') : wp_title(''); ?> | <?php bloginfo('name'); ?></title>
+  <title><? wp_title('') . " | " . bloginfo('name') . " - " . bloginfo('description'); ?></title>
   <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
   <?php wp_head(); ?>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
