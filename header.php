175f636ec1b17187cd07c805d302854673bab3c6
Henry HTPC <henry@henryhtpc.homeip.net>
2014-11-11 10:18:12 +0000

    header.php: add chrome color-theme support.

--- header.php
+++ header.php
@@ -3,12 +3,13 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#0059B2">
   <title><?php wp_title('|'); ?></title>
   <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
   <?php wp_head(); ?>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 </head>
-  
+
 <body <?php body_class(); ?>>
 
 <div id="header">
@@ -16,10 +17,10 @@
   <div id="header-menu">
     <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
   </div>
-  
-  <?php 
+
+  <?php
     global $post;
-	  $parents = get_post_ancestors( $post->ID );
+          $parents = get_post_ancestors( $post->ID );
     $id = ($parents) ? $parents[count($parents)-1]: $post->ID;
   ?>
 
